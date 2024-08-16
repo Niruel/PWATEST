cd977cfa0604a1a4e5a91cce139eb6f23d13ecd3
Niruel <npruppel@gmail.com>
2024-08-16 13:41:36 -0500

    update without device tag

--- Build/WebGL Builds.framework.js
+++ Build/WebGL Builds.framework.js
@@ -1993,13 +1993,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4055440: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4055501: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4055565: function() {return Module.webglContextAttributes.powerPreference;},  
- 4055623: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+  4055392: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4055453: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4055517: function() {return Module.webglContextAttributes.powerPreference;},  
+ 4055575: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4055630: function($0) {performance.now = function() { return $0; };},  
  4055678: function($0) {performance.now = function() { return $0; };},  
- 4055726: function($0) {performance.now = function() { return $0; };},  
- 4055774: function() {performance.now = Module['emscripten_get_now_backup'];}
+ 4055726: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
